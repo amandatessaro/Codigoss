4582081a27aec528b0ff441d5bf1db16710e3c81
amandatessaro <amanda.franca00983031@sesisenaipr.org.br>
2025-10-23 17:05:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function somarNumeros(){
+    let n1 = Number(document.getElementById("n1").value)
+    let n2 = Number(document.getElementById("n2").value)
+    alert(n1+n2);
+}
+
+function subtrairNumeros(){
+    let n1 = Number(document.getElementById("n1").value)
+    let n2 = Number(document.getElementById("n2").value)
+    alert(n1-n2);
+}
+
+function multiplicarNumeros(){
+    let n1 = Number(document.getElementById("n1").value)
+    let n2 = Number(document.getElementById("n2").value)
+    alert(n1*n2);
+}
+
+function dividirNumeros(){
+    let n1 = Number(document.getElementById("n1").value)
+    let n2 = Number(document.getElementById("n2").value)
+    alert(n1/n2);
+}
