2100132904a8f411f475f2a6296aa3eef3b3f2e0
amandatessaro <amanda.franca00983031@sesisenaipr.org.br>
2025-11-18 16:50:58 -0300

    Update login.php

--- aula.htdos/login.php
+++ aula.htdos/login.php
@@ -1,8 +1,40 @@
-<from action="">
-    <input type="text" placeholder="usuario">
-    <input type="text" placeholder="senha">
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="login.css">
+</head>
 
+<body>
+    <div class="container flex centro fundo">
+        <div class="quadrado-login flex centro">
+            <div class="quadrado flex centro coluna letra cor">
+                <h1> Realize o seu login! </h1>
+                <br>
+                <br>
+                <br>
+                <div class="flex coluna">
+                    <div class="input-group">
+                        <input type="email" placeholder=" " class="acessar espaco" name="email" required>
+                        <label>Email</label>
+                    </div>
+                    <br>
+                    <div class="input-group">
+                        <input type="password" placeholder=" " class="acessar espaco" name="senha" required>
+                        <label>Senha</label>
+                    </div>
+                </div>
+                <br>
+                <button class="botaoo" role="button" onclick="login()"> Entrar no sistema </button>
+            </div>
+        
 
-    <input type="submit" value="Logar">
-</from>
+        </div>
+    </div>
+    <script src="script.js"></script>
+</body>
+
+</html>
